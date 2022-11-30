e7596bc6670844b6f5161329b759992017a7d938
Your Name <you@example.com>
2022-11-30 13:35:37 +0300

    fast

--- code.py
+++ code.py
@@ -8,13 +8,15 @@ win_width = 1300
 win = pygame.display.set_mode((win_width,win_height))
 pygame.display.set_caption("Swowmen Fight")
 
-
-width = 20
-height = 30
+score = 0
+width = 145
+height = 158
 x = random.randrange(0,win_width - width,1)
 y=random.randrange(0,win_height - height,1)
-speed = 0.8
+speed = 2
 run = True
+
+
 
 bg = pygame.image.load('images/фон.jpg')
 snowman = pygame.image.load('images/снеговик175.png')
@@ -34,11 +36,11 @@ while run:
         if event.type == pygame.QUIT:
             run = False
     keys = pygame.key.get_pressed()
-    if keys[pygame.K_LEFT] and x>0:
+    if keys[pygame.K_LEFT] and x>0-25:
         x -=speed
     if keys[pygame.K_RIGHT]and x<1300 - width:
         x +=speed
-    if keys[pygame.K_UP] and y>0:
+    if keys[pygame.K_UP] and y>0-22:
         y =y -speed
     if keys[pygame.K_DOWN] and y<750 - height:
         y +=speed
